a1b143cd7f65f768df5ecae4e25d905f41340df9
Amani Atiah <amaniatiah@gmail.com>
2019-10-26 17:31:15 +0300

    add images

--- app/src/main/java/com/barmej/gameoftraditionalwords/MainActivity.java
+++ app/src/main/java/com/barmej/gameoftraditionalwords/MainActivity.java
@@ -1,14 +1,62 @@
 package com.barmej.gameoftraditionalwords;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageViewQuestion;
+
+    private int IMAGE [] = {
+            R.drawable.icon_1,
+            R.drawable.icon_2,
+            R.drawable.icon_3,
+            R.drawable.icon_4,
+            R.drawable.icon_5,
+            R.drawable.icon_6,
+            R.drawable.icon_7,
+            R.drawable.icon_8,
+            R.drawable.icon_9,
+            R.drawable.icon_10,
+            R.drawable.icon_11,
+            R.drawable.icon_12,
+            R.drawable.icon_13
+    };
+
+    private String[] ANSWERS;
+    private String[] ANSWERS_DESCRIPTION;
+
+    private String mCurrentAnswer, mCurrentAnswerDescription;
+    private int mCurrentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageViewQuestion = findViewById(R.id.image_view_question);
+        ANSWERS = getResources().getStringArray(R.array.answers);
+        ANSWERS_DESCRIPTION = getResources().getStringArray(R.array.answer_description);
+        showNewImage();
+
+    }
+
+    private void showNewImage() {
+        Random random = new Random();
+        int randomImageIndex = random.nextInt(IMAGE.length);
+        mCurrentImage = IMAGE[randomImageIndex];
+        mCurrentAnswer = ANSWERS[randomImageIndex];
+        mCurrentAnswerDescription = ANSWERS_DESCRIPTION[randomImageIndex];
+        Drawable placeDrawable = ContextCompat.getDrawable(this,mCurrentImage);
+        imageViewQuestion.setImageDrawable(placeDrawable);
+    }
+
+    public void onChangeImageClicked(View view) {
+        showNewImage();
     }
 }

e54f1865d4334e2ccf8a65ad7a09076de069836b
Amani Atiah <amaniatiah@gmail.com>
2019-11-03 20:50:39 +0300

    edit project

--- app/src/main/java/com/barmej/gameoftraditionalwords/MainActivity.java
+++ app/src/main/java/com/barmej/gameoftraditionalwords/MainActivity.java
@@ -26,7 +26,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_WRITE_EXTERNAL_STORAGE =123 ;
     public ImageView imageViewPicture;
-    private ImageButton changeLang;
+    public ImageButton changeLang;
 
     private int IMAGE [] = {
             R.drawable.icon_1,
@@ -111,7 +111,66 @@ public class MainActivity extends AppCompatActivity {
         Drawable placeDrawable = ContextCompat.getDrawable(this,mCurrentImage);
         imageViewPicture.setImageDrawable(placeDrawable);
     }
+    private void checkPermissionAndShare() {
+        // insertImage في النسخ من آندرويد 6 إلى آندرويد 9 يجب طلب الصلاحية لكي نتمكن من استخدام الدالة
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // هنا لا يوجد صلاحية للتطبيق ويجب علينا طلب الصلاحية منه عن طريك الكود التالي
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // بسبب عدم منح المستخدم الصلاحية للتطبيق فمن الأفضل شرح فائدتها له عن طريق عرض رسالة تشرح ذلك
+                // هنا نقوم بإنشاء AlertDialog لعرض رسالة تشرح للمستخدم فائدة منح الصلاحية
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setTitle(R.string.permission_title)
+                        .setMessage(R.string.permission_explanation)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // requestPermissions عند الضغط على زر منح نقوم بطلب الصلاحية عن طريق الدالة
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+                            }
+                        }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //  عند الضغط على زر منع نقوم بإخفاء الرسالة وكأن شيء لم يكن
+                                dialogInterface.dismiss();
+                            }
+                        }).create();
 
+                // نقوم بإظهار الرسالة بعد إنشاء alertDialog
+                alertDialog.show();
+
+            } else {
+                // لا داعي لشرح فائدة الصلاحية للمستخدم ويمكننا طلب الصلاحية منه
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+            }
+
+        } else {
+            // الصلاحية ممنوحه مسبقا لذلك يمكننا مشاركة الصورة
+            shareImage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // يتم استدعاء هذه الدالة بعد اختيار المستخدم احد الخيارين من رسالة طلب الصلاحية
+        if (requestCode == PERMISSIONS_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // تم منح الصلاحية من قبل المستخدم لذلك يمكننا مشاركة الصورة الآن
+                shareImage();
+            } else {
+                // لم يتم منح الصلاحية من المستخدم لذلك لن نقوم بمشاركة الصورة، طبعا يمكننا تنبيه المستخدم بأنه لن يتم مشاركة الصورة لسبب عدم منح الصلاحية للتطبيق
+                Toast.makeText(MainActivity.this, R.string.permission_explanation, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
     public void onChangeImageClicked(View view) {
         shareImage();
     }
